399d79f11ce70e8276af01e1ded4f77445374391
UO264699 <Andresfdez9@gmail.com>
2022-05-06 01:35:00 +0200

    login

--- gpgs/src/app/app.module.ts
+++ gpgs/src/app/app.module.ts
@@ -6,11 +6,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import  {MatIconModule} from '@angular/material/icon';
-import  {MatFormFieldModule} from '@angular/material/form-field';
+import  {FormsModule} from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { LoginEmpresaComponent } from './login-empresa/login-empresa.component';
 import { LoginClienteComponent } from './login-cliente/login-cliente.component';
 import { FooterComponent } from './footer/footer.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
@@ -25,9 +28,12 @@ import { FooterComponent } from './footer/footer.component';
     BrowserModule,
     BrowserAnimationsModule,
     MatIconModule,
-    MatFormFieldModule,
+    FormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatMenuModule,
     RouterModule.forRoot([
-      { path: '', component: LoginComponent },
+      { path: 'login', component: LoginClienteComponent },
     ])
   ],
   providers: [],
--- gpgs/src/app/login-cliente/login-cliente.component.ts
+++ gpgs/src/app/login-cliente/login-cliente.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginClienteComponent implements OnInit {
 
-  constructor() { }
+  hide = true;
+
+  constructor() {
+   
+   }
 
   ngOnInit(): void {
+
+
   }
+
+
 
 }
