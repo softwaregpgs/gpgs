b1daf0c63472e259b449b3fa57cf45974f05893e
UO264699 <Andresfdez9@gmail.com>
2022-05-05 20:32:23 +0200

    Toolbar y navegación

--- gpgs/src/app/app.module.ts
+++ gpgs/src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BotonComponent } from './boton/boton.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import  {MatIconModule} from '@angular/material/icon';
+import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BotonComponent
+    BotonComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    RouterModule.forRoot([
+      { path: 'login', component: LoginComponent },
+      { path: '', component: BotonComponent },
+    ])
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  
 })
 export class AppModule { }
