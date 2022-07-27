e33127834c817e3ff1123afcdf6604584abfe14b
andresfdezglez <Andresfpano@gmail.com>
2022-07-27 20:44:37 +0200

    Lista de empresas a medias

--- gpgs/src/app/app.module.ts
+++ gpgs/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
